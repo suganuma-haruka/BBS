521273659670ee1cb1c6b8f2c48b58a5e1652fb2
haruka suganuma <haruka.b26@gmail.com>
2016-07-29 16:36:05 +0900

    commit

--- BBS/view.sql
+++ BBS/view.sql
@@ -1,15 +1,41 @@
-create view user_posting as
-select
-	postings.id as posting_id,
-	postings.user_id as user_id,
-	branch_id,
-	position_id,
-	name,
-	title,
-	postings.text as text,
-	category,
-	postings.insert_date as insert_date
-from
-	users, postings,
-where
-	users.id = postings.user_id;
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `bbs`.`user_posting` AS
+    select 
+        `bbs`.`postings`.`id` AS `posting_id`,
+        `bbs`.`postings`.`user_id` AS `user_id`,
+        `bbs`.`users`.`branch_id` AS `branch_id`,
+        `bbs`.`users`.`position_id` AS `position_id`,
+        `bbs`.`users`.`name` AS `name`,
+        `bbs`.`postings`.`title` AS `title`,
+        `bbs`.`postings`.`text` AS `text`,
+        `bbs`.`postings`.`category` AS `category`,
+        `bbs`.`postings`.`insert_date` AS `insert_date`
+    from
+        (`bbs`.`users`
+        join `bbs`.`postings`)
+    where
+        (`bbs`.`users`.`id` = `bbs`.`postings`.`user_id`)
+        
+
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `bbs`.`user_comment` AS
+    select 
+        `bbs`.`comments`.`id` AS `comment_id`,
+        `bbs`.`comments`.`posting_id` AS `posting_id`,
+        `bbs`.`comments`.`user_id` AS `user_id`,
+        `bbs`.`users`.`branch_id` AS `branch_id`,
+        `bbs`.`users`.`position_id` AS `position_id`,
+        `bbs`.`users`.`name` AS `name`,
+        `bbs`.`comments`.`text` AS `text`,
+        `bbs`.`comments`.`insert_date` AS `insert_date`
+    from
+        (`bbs`.`users`
+        join `bbs`.`comments`)
+    where
+        (`bbs`.`users`.`id` = `bbs`.`comments`.`user_id`)
