3bb5749e1a24fb19960b43d014e791051be8a519
haruka suganuma <haruka.b26@gmail.com>
2016-07-29 16:21:56 +0900

    commit

---
+++ Chapter2/src/chapter2/HelloWorldServlet.java
@@ -0,0 +1,41 @@
+package chapter2;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/HelloWorld" })
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		try {
+			System.out.println("BEGIN Hello, World! " + getCurrentTime());
+
+			//リクエストにメッセージをセット
+//			req.setAttribute("message", "Hello, World!");
+			req.setAttribute("message", getCurrentTime());
+
+			RequestDispatcher dispatcher = req
+					.getRequestDispatcher("/HelloWorld.jsp");
+
+			dispatcher.forward(req, resp);
+		}finally {
+			System.out.println("END   Hello, World! " + getCurrentTime());
+		}
+	}
+
+	private String getCurrentTime() {
+		return new SimpleDateFormat("HH:mm:ss").format(new Date());
+	}
+}
